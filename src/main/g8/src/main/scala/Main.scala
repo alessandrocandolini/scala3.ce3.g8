293ff2d9a84f4f4dee1449b0955f61566051920a
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-08-30 15:09:57 +0100

    decline and opts

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,4 +1,26 @@
 import cats.effect.{IO, IOApp}
+import cats.effect.std.Console
+import cats.Show
+import cli._
+import com.monovore.decline.Opts
+import fs2._
+import cats.Functor
 
-object Main extends IOApp.Simple:
-  val run = IO.println("Hello world!")
+object Main extends CommandIOAppSimple(
+      name = "simple cli",
+      header = "simple cli",
+      version = "0.1"
+    ):
+
+  override def run: Opts[IO[Unit]] = Args.readArgs.map(program)
+
+  def source : Stream[Pure, String] = Stream("hello", "world")
+
+  def pipeline[F[_] : Console : Functor, A : Show] : Stream[F,A] => Stream[F,A] =
+    _.evalTap(Console[F].println)
+
+  val program: Args => IO[Unit] = _ =>
+    source.covary[IO]
+      .through(pipeline)
+      .compile
+      .drain
